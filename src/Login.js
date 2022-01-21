7972e3cd9aae4ae9fc3ff554c4ecc33547f0bbcb
Pratap Singh <65844506+DSPapsa@users.noreply.github.com>
2022-01-21 01:27:45 -0800

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,39 +1,55 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom';
-import './Login.css'
+import React, { useState } from 'react';
+import './Register.css'
+import {Link} from 'react-router-dom'
 
-function Login() {
+
+function Register() {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const signIn = e => {
-        e.preventDefault()
-    }
-    const registerButton = e => {
-        e.preventDefault()
-        
-    }
+
     return (
-        <div className='login'>
-            <Link to='/'>
-            <img className='login_logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' alt=""/>
-            </Link>
-            <div className='login_container'>
-                <h1>Sign-in</h1>
+        <div className='register'>
+
+            <img
+                className="register__logo"
+                src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' alt='' />
+
+
+            <div className='register__container'>
+                <h1>Create Account</h1>
+
                 <form>
-                    <h5>E-mail</h5>
-                    <input type='text' value={email} onChange={ e => setEmail(e.target.value)} />
+                    <h5>Name</h5>
+                    <input type='text' value={name} onChange={e => setName(e.target.value)} />
+                    <h5> E-mail</h5>
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type='password' value={password} onChange={ e => setPassword(e.target.value)}/>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button type='submit' onClick={signIn} className='login_signInButton'>Sign-in</button>
+                    <Link to='/login'><button type='submit'  className='register_signupButton'>Sign-Up</button></Link>
                 </form>
-                <p>Digikull's group project E-Commerce website . Please
-             see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.</p>
-                    <button onClick={registerButton} onclassName='login_registerButton'>Create your Amazon Account</button>
+                <h3>Other Sign-up Options</h3>
+
+
+                <img className='signup_logo' src="https://scontent.fudr3-1.fna.fbcdn.net/v/t39.2365-6/17639236_1785253958471956_282550797298827264_n.png?_nc_cat=105&ccb=1-5&_nc_sid=ad8a9d&_nc_ohc=Oomt0XkspmkAX8pqiIl&_nc_ht=scontent.fudr3-1.fna&oh=00_AT9b5vB1lYRO2UQjtquihqPyO4oJD2LZKFl8GX1OSv5FWw&oe=61EDFF96" alt='' />
+                <div class="google-btn">
+                    <div class="google-icon-wrapper">
+                        <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt='' />
+                    </div>
+                    <p class="btn-text"><b>Sign in with google</b></p>
+                </div>
+
+                <p>
+                Digikull's group project E-Commerce website . Please
+             see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+
+
             </div>
-        </div>
+        </div >
     )
 }
 
-export default Login
+export default Register
